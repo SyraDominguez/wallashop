8e29b26180a6e4d564c122c538bfe74912c3fd79
Syra Dominguez <syradominguez.dev@gmail.com>
2024-03-24 14:08:26 +0100

    Added protection for creation page

--- ad-creation.js
+++ ad-creation.js
@@ -2,6 +2,14 @@ import { adCreationController } from "./ads-creation/ad-creation-controller.js";
 
 
 document.addEventListener('DOMContentLoaded', () => {
+  const token = localStorage.getItem('token');
+
+  if (!token) {
+    alert('You must to login first to create a new ad')
+    window.location.href = ('/');
+  }
+
   const adCreation = document.querySelector('#adCreation');
   adCreationController(adCreation);
 });
+
